667d9e70cd804ed4b344f8af3b541e1670d8e512
Glen Pierce <glen.piercev@gmail.com>
2016-03-05 11:56:00 -0800

    node.js twilio api

--- twilioapi.js
+++ twilioapi.js
@@ -1,6 +1,10 @@
+var twilioClient = require('../twilioClient');
+
 cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
 cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
 cfg.sendingNumber = process.env.TWILIO_NUMBER;
+
+var client = require('twilio')(cfg.accountSid, cfg.authToken);
 
 var requiredConfig = [cfg.accountSid, cfg.authToken, cfg.sendingNumber];
 var isConfigured = requiredConfig.every(function(configValue) {
@@ -14,5 +18,20 @@ if (!isConfigured) {
   throw new Error(errorMessage);
 }
 
-// Export configuration object
-module.exports = cfg;
+sendSms = function(to, message) {
+  client.messages.create({
+    body: message,
+    to: to,
+    from: config.sendingNumber
+    // mediaUrl: 'http://www.yourserver.com/someimage.png'
+  }, function(err, data) {
+    if (err) {
+      console.error('Could not notify administrator');
+      console.error(err);
+    } else {
+      console.log('Administrator notified');
+    }
+  });
+};
+
+twilioClient.sendSms(phoneNumber, messageToSend);
